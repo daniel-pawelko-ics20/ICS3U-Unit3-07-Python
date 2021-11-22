91794e694c63eb8a82bef7ec934f772319149c82
Ubuntu <ubuntu@ip-172-31-85-153.ec2.internal>
2021-11-22 16:38:37 +0000

    Finished

--- main.py
+++ main.py
@@ -10,16 +10,16 @@ def main():
 
     # asking for input
     try:
-        age = int(input("Please enter your age: "))
+        age = input("Please enter your age: ")
+        age = int(age)
+        
+        # process/output
+        if age > 40 or age < 25:
+            print("You are NOT an acceptable age")
+        else:
+            print("You are an appropriate age")
     except ValueError:
         print("Input has to be integer")
-        return
-
-    # process/output
-    if age > 40 or age < 25:
-        print("You are NOT an acceptable age")
-    else:
-        print("You are an appropriate age")
 
     # done
     print("\nDone.")
